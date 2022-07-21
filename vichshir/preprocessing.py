78624f5adb60df7805f5daeb65c4fd4d56d1c39e
vichShir <victor.shirasuna@gmail.com>
2022-07-21 15:09:12 -0300

    Add impute missing data function in preprocessing

--- vichshir/preprocessing.py
+++ vichshir/preprocessing.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
+from pandas.api.types import is_string_dtype
+from pandas.api.types import is_numeric_dtype
 
 def normalize(dataframe:pd.DataFrame, columns:list=None, by_dtype:list=None, inplace:bool=False):
   """ Normalize columns given a pandas dataframe using MinMaxScaler
@@ -32,3 +34,32 @@ def normalize(dataframe:pd.DataFrame, columns:list=None, by_dtype:list=None, inp
       for column in dataframe.columns:
         if dataframe[column].dtype in by_dtype:
           dataframe[column] = min_max_scaler.fit_transform(np.array(dataframe[column]).reshape(-1, 1))
+
+
+def impute_missing_data(dataframe, columns:list=None, fill_method=np.nanmedian, inplace=False):
+  """ Fill missing data
+  dataframe: Pandas DataFrame
+  columns: list of columns
+  fill_method: numpy method for numerical columns
+  inplace: return dataframe
+  """
+
+  df = dataframe.copy()
+
+  if inplace == False:
+    for column in columns:
+      if is_numeric_dtype(df[column]):
+        col_stats = fill_method(df[column]) # mean, median, mode...
+        df[column] = df[column].fillna(col_stats)
+      elif is_string_dtype(df[column]):
+        col_mode = df[column].value_counts().index[0] # mode
+        df[column] = df[column].fillna(col_mode)
+    return df
+  else:
+    for column in columns:
+      if is_numeric_dtype(dataframe[column]):
+        col_stats = fill_method(dataframe[column]) # mean, median, mode...
+        dataframe[column] = dataframe[column].fillna(col_stats)
+      elif is_string_dtype(dataframe[column]):
+        col_mode = dataframe[column].value_counts().index[0] # mode
+        dataframe[column] = dataframe[column].fillna(col_mode)
