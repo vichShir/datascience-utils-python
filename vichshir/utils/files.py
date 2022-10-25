fd1e51aac8cb0ce9f3ebc4093d7585df29a5b890
vichShir <victor.shirasuna@gmail.com>
2022-10-25 09:25:34 -0300

    add files util module

---
+++ vichshir/utils/files.py
@@ -0,0 +1,26 @@
+import os
+import requests
+
+
+def download(url: str, filename: str, dest_folder: str = './'):
+    """
+    Credits to: Ivan Vinogradov
+    From: https://stackoverflow.com/questions/56950987/download-file-from-url-and-save-it-in-a-folder-python
+    """
+
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)  # create folder if it does not exist
+
+    file_path = os.path.join(dest_folder, filename)
+
+    r = requests.get(url, stream=True)
+    if r.ok:
+        print("saving to", os.path.abspath(file_path))
+        with open(file_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024 * 8):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+                    os.fsync(f.fileno())
+    else:  # HTTP status code 4XX/5XX
+        print("Download failed: status code {}\n{}".format(r.status_code, r.text))
